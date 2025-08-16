64fb823579400db600213a7fefe8043da32f175c
daniela392 <moisesalejandrolopezgomez741@gmail.com>
2025-08-16 03:25:19 -0500

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -17,11 +17,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `*\n *╭━〔👑 *ASTA-BOT* 👑  〕* \n *┋* \n *┋「 🎉 *¡BIENVENIDO/A!* 👋 」* \n *┋* \n *┋* 「 ${groupMetadata.subject} 」 \n *┋* \n *╰━★ 「*.   @${m.messageStubParameters[0].split`@`[0]} . *」* \n *╭━━━━━━ * \n *┋❖ Le las reglas de la descricion* \n *┋❀ Espero que te la lleves bien * \n *┋❖ Haora somos* ${groupSize} \n *┗━━━━━━━━━━━━━━━┅ ⳹* \n  ${global.welcom2}`    
+    let bienvenida = `*\n *╭━〔👑 *ASTA-BOT* 👑  〕* \n *┋* \n *┋「 🎉 *¡BIENVENIDO/A!* 👋 」* \n *┋* \n *┋* 「 ${groupMetadata.subject} 」 \n *┋* \n *╰━★ 「*.   @${m.messageStubParameters[0].split`@`[0]} . *」* \n *╭━━━━━━ * \n *┋❖ Le las reglas de la descricion* \n *┋❀ Espero que te la lleves bien * \n *┋❖ ahora somos* ${groupSize} \n *┗━━━━━━━━━━━━━━━┅ ⳹* \n  ${global.welcom2}`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `* *╭━〔👑 *ASTA-BOT* 👑  〕* \n *┋* \n *┋「  *¡ADIOS!* 👋 」* \n *┋* \n *┋* 「 ${groupMetadata.subject} 」 \n *┋* \n *╰━★ 「*.   @${m.messageStubParameters[0].split`@`[0]} . *」* \n *╭━━━━━━ * \n *┋❖ Que bueno se fue un mas* \n *┋❀ Jamás te quisimos aquí* \n *┋❖ Haora somos* ${groupSize} \n *┗━━━━━━━━━━━━━━━┅ ⳹* \n  ${global.welcom2}`
+    let bye = `* *╭━〔👑 *ASTA-BOT* 👑  〕* \n *┋* \n *┋「  *¡ADIOS!* 👋 」* \n *┋* \n *┋* 「 ${groupMetadata.subject} 」 \n *┋* \n *╰━★ 「*.   @${m.messageStubParameters[0].split`@`[0]} . *」* \n *╭━━━━━━ * \n *┋❖ Que bueno se fue un mas* \n *┋❀ Jamás te quisimos aquí perro* \n *┋❖ ahora somos* ${groupSize} \n *┗━━━━━━━━━━━━━━━┅ ⳹* \n  ${global.welcom2}`
     await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak)
   }}
