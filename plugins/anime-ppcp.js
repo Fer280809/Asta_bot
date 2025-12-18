4ada9214fa1b9a906eeb4619b3c9db625b75a9d3
Víctor Manuel | Orion's Wolf <vm1008079@gmail.com>
2025-12-18 16:35:48 -0500

    Update anime-ppcp.js

--- plugins/anime-ppcp.js
+++ plugins/anime-ppcp.js
@@ -1,18 +1,18 @@
 import fetch from "node-fetch"
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, usedPrefix }) => {
 try {
-await m.react('🕒')
+await m.react('🎄')
 let data = await (await fetch('https://raw.githubusercontent.com/ShirokamiRyzen/WAbot-DB/main/fitur_db/ppcp.json')).json()
 let cita = data[Math.floor(Math.random() * data.length)]
 let cowi = await (await fetch(cita.cowo)).buffer()
-await conn.sendFile(m.chat, cowi, '', '*Masculino* ♂', m)
+await conn.sendFile(m.chat, cowi, '', '🎅 *Reno Masculino* 🦌', m)
 let ciwi = await (await fetch(cita.cewe)).buffer()
-await conn.sendFile(m.chat, ciwi, '', '*Femenina* ♀', m)
-await m.react('✔️')
+await conn.sendFile(m.chat, ciwi, '', '🎀 *Elfa Femenina* 🎁', m)
+await m.react('✨')
 } catch (error) {
-await m.react('✖️')
-await conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${error.message}`, m)
+await m.react('❄️')
+await conn.reply(m.chat, `🎄 *¡Oh no!* Algo salió mal en el taller.\n> Usa *${usedPrefix}report* para informar a Santa.\n\n${error.message}`, m)
 }}
 
 handler.help = ['ppcouple']
