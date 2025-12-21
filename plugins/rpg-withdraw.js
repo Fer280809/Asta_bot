ab5f680d348f6c3aa2efd03bae849a040c5fcd0a
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer280809@users.noreply.github.com>
2025-12-20 19:52:35 -0600

    Actualizar rpg-withdraw.js

--- plugins/rpg-withdraw.js
+++ plugins/rpg-withdraw.js
@@ -1,28 +1,28 @@
 let handler = async (m, { args, usedPrefix, command }) => {
     if (!db.data.chats[m.chat].economy && m.isGroup) {
-        return m.reply(`⚠️ Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con:\n» *${usedPrefix}economy on*`)
+        return m.reply(`🎄 *¡Oh no!* Los regalos económicos están *congelados* en este grupo navideño.\n\n🎅 Un *elfo administrador* puede descongelarlos con:\n» *${usedPrefix}economy on*`)
     }
     let user = global.db.data.users[m.sender]
-    if (!args[0]) return m.reply(`❗ Ingresa la cantidad de *${currency}* que deseas retirar.`)
+    if (!args[0]) return m.reply(`🎁 *¡Hola!* Por favor, escribe la cantidad de *${currency}* que quieres retirar de tu cuenta navideña.`)
 
     if (args[0] === 'all') {
         let count = parseInt(user.bank)
-        if (!count) return m.reply(`❌ No tienes *${currency}* para retirar.`)
+        if (!count) return m.reply(`❄️ *¡Vaya!* No tienes *${currency}* en tu cuenta navideña para retirar.`)
         user.bank -= count
         user.coin += count
-        await m.reply(`✅ Has retirado *¥${count.toLocaleString()} ${currency}* del banco.\n> Ahora puedes usarlo libremente, ¡pero cuidado con los robos!`)
+        await m.reply(`🎅 *¡Perfecto!* Has retirado *¥${count.toLocaleString()} ${currency}* de tu cuenta navideña.\n> Ahora puedes usarlo para comprar regalos, ¡pero ten cuidado con los Grinches!`)
         return
     }
 
-    if (!Number(args[0])) return m.reply(`❌ Cantidad inválida.\n> Ejemplo 1 » *${usedPrefix + command} 25000*\n> Ejemplo 2 » *${usedPrefix + command} all*`)
+    if (!Number(args[0])) return m.reply(`🎄 *Cantidad inválida.*\n> Ejemplo 1 » *${usedPrefix + command} 25000*\n> Ejemplo 2 » *${usedPrefix + command} all*`)
 
     let count = parseInt(args[0])
-    if (!user.bank) return m.reply(`❌ No tienes fondos en el Banco.`)
-    if (user.bank < count) return m.reply(`⚠️ Solo tienes *¥${user.bank.toLocaleString()} ${currency}* en el Banco.`)
+    if (!user.bank) return m.reply(`❄️ *¡Oh!* No tienes fondos en tu cuenta navideña.`)
+    if (user.bank < count) return m.reply(`🎁 *¡Cuidado!* Solo tienes *¥${user.bank.toLocaleString()} ${currency}* en tu cuenta navideña.`)
 
     user.bank -= count
     user.coin += count
-    await m.reply(`✅ Has retirado *¥${count.toLocaleString()} ${currency}* del banco.\n> Ahora puedes usarlo, ¡pero cuidado con los robos!`)
+    await m.reply(`🎅 *¡Excelente!* Has retirado *¥${count.toLocaleString()} ${currency}* de tu cuenta navideña.\n> Ahora puedes usarlo para comprar regalos, ¡pero ten cuidado con los Grinches!`)
 }
 
 handler.help = ['retirar']
