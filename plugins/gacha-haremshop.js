041d45b7398f982e5bf803e2e51f273e86cf8bc8
Víctor Manuel | Orion's Wolf <vm1008079@gmail.com>
2025-12-23 19:09:38 -0500

    Actualizar gacha-haremshop.js

--- plugins/gacha-haremshop.js
+++ plugins/gacha-haremshop.js
@@ -115,7 +115,7 @@ const handler = async (m, { conn, args }) => {
 
 handler.help = ['haremshop', 'tienda', 'market', 'mercadonavideño'];
 handler.tags = ['gacha', 'navidad', 'economy'];
-handler.command = ['haremshop', 'tienda', 'market', 'tiendagacha'];
+handler.command = ['gachashop', 'tiendagacha'];
 handler.group = true;
 handler.private = true;
 
